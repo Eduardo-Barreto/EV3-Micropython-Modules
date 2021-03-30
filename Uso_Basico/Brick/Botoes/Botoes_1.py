da9d28cde4ce8a1d15c4333c2869e3720f525a46
Eduardo-Barreto <eduwagzozo@gmail.com>
2021-03-29 23:42:38 -0300

    espaçamento de fonte corrigido

--- Uso_Basico/Brick/Botoes/Botoes_1.py
+++ Uso_Basico/Brick/Botoes/Botoes_1.py
@@ -54,6 +54,5 @@ if __name__ == '__main__':
     # Executa nossa função de teste
     main()
 
-
 # Fonte:
 # https://pybricks.github.io/ev3-micropython/hubs.html#pybricks.hubs.EV3Brick.buttons.pressed
